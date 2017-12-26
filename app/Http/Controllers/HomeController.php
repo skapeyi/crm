f98000e0a90f00bc47ccc4260209d064f9d3649a
Samson Kapeyi <skapeyi@bitbucket.org>
2017-12-26 13:26:37 +0300

    Added some stats on the front page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Log;
+use Carbon\Carbon;
+use App\Subscriber;
+use App\Organization;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,7 +27,30 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {       
-        return view('home');
+    { 
+        $today = Carbon::now();
+        $one_month_after = $today->addMonths(1)->toDateString();
+        $total_members = DB::table('subscribers')->count();
+        $total_organizations = DB::table('organizations')->count(); 
+
+        $members_expiring_query = DB::table('subscribers as s')
+                                ->join('subscriber_payments as sp','s.id','=','sp.subscriber_id')
+                                ->select('s.name', 's.email','s.phone','sp.payment_date','sp.expiry_date')
+                                ->whereDate('sp.expiry_date','<=',$one_month_after)
+                                ->orderBy('sp.expiry_date','ASC')
+                                ->groupBy('s.email');       
+        
+        $members_expiring_soon = $members_expiring_query->simplePaginate(10,['*'],'members');
+
+        $members_expiring_query = DB::table('organizations as o')
+                                ->join('organization_payments as op','o.id','=','op.organization_id')
+                                ->select('o.name', 'o.email','o.phone','op.payment_date','op.expiry_date')
+                                ->whereDate('op.expiry_date','<=',$one_month_after)
+                                ->orderBy('op.expiry_date','ASC')
+                                ->groupBy('o.email');       
+        
+        $organizations_expiring_soon = $members_expiring_query->simplePaginate(10,['*'],'organizations');
+
+        return view('home', compact('total_members','total_organizations','members_expiring_soon','organizations_expiring_soon'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,132 @@
 @section('title', 'ICTAU CRM')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+<h1>Dashboard</h1>
 @stop
 
 @section('content')
-    <p>You are logged in!</p>
+<div class="row">
+	<div class="col-md-3">
+		<div class="panel panel-primary">
+			<div class="panel-body">
+				<h1>{{$total_members}}</h1>
+			</div>
+			<div class="panel-footer">
+				Total Members
+			</div>
+		</div>    	
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-danger">
+			<div class="panel-body">
+				<h1>{{count($members_expiring_soon)}}</h1>
+			</div>
+			<div class="panel-footer">
+				Total Members Expiring in 30 days
+			</div>
+		</div>    	
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-primary">
+			<div class="panel-body">
+				<h1>{{$total_organizations}}</h1>
+			</div>
+			<div class="panel-footer">
+				Total Organizations
+			</div>
+		</div>    	
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-danger">
+			<div class="panel-body">
+				<h1>{{count($organizations_expiring_soon)}}</h1>
+			</div>
+			<div class="panel-footer">
+				Total Organizations Expiring in 30 days
+			</div>
+		</div>    	
+	</div>
+
+</div>
+<div class="row">
+	<div class="col-md-6">
+		<div class="panel panel-info">
+			<div class="panel-header">
+
+			</div>
+			<div class="panel-body">
+				@if(count($members_expiring_soon) < 0 )
+				<p>You have no subscriptions ending in the next 30 days</p>
+				@else
+				<div class="table-responsive">
+					<table class="table table-stripped table-hover">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Phone</th>
+								<th>Payment Date</th>
+								<th>Expiration Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($members_expiring_soon as $item)
+							<tr>
+								<td>{{$item->name}}</td>
+								<td>{{$item->email}}</td>
+								<td>{{$item->phone}}</td>
+								<td>{{$item->payment_date}}</td>
+								<td>{{$item->expiry_date}}</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+
+				</div>
+				{{ $members_expiring_soon->links() }}
+				@endif
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="panel panel-info">
+			<div class="panel-header">
+
+			</div>
+			<div class="panel-body">
+				@if(count($organizations_expiring_soon) < 0 )
+				<p>You have no organization subscriptions ending in the next 30 days</p>
+				@else
+				<div class="table-responsive">
+					<table class="table table-stripped table-hover">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Phone</th>
+								<th>Payment Date</th>
+								<th>Expiration Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($organizations_expiring_soon as $item)
+							<tr>
+								<td>{{$item->name}}</td>
+								<td>{{$item->email}}</td>
+								<td>{{$item->phone}}</td>
+								<td>{{$item->payment_date}}</td>
+								<td>{{$item->expiry_date}}</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+
+				</div>
+				{{ $organizations_expiring_soon->links() }}
+				@endif
+			</div>
+		</div>
+	</div>
+</div>
 @stop
