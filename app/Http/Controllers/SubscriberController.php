b2b4764aaaabb9cc538f483119cacf3d7990b4e3
Samson Kapeyi <skapeyi@bitbucket.org>
2018-01-10 13:02:08 +0300

    Fixed error with query for members expiring soon

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -200,15 +200,16 @@ class SubscriberController extends Controller
     public function expiringSoon(){
         $today = Carbon::now();
         $one_month_after = $today->addMonths(1)->toDateString();
-         $organizations_expiring_query = DB::table('organizations as o')
-                                ->join('organization_payments as op','o.id','=','op.organization_id')
-                                ->select('o.name', 'o.email','o.phone','op.payment_date','op.expiry_date')
-                                ->whereDate('op.expiry_date','<=',$one_month_after)
-                                ->orderBy('op.expiry_date','ASC')
-                                ->groupBy('o.email');       
+        $members_expiring_query = DB::table('subscribers as s')
+                                ->join('subscriber_payments as sp','s.id','=','sp.subscriber_id')
+                                ->select('s.name', 's.email','s.phone','sp.payment_date','sp.expiry_date')
+                                ->whereDate('sp.expiry_date','<=',$one_month_after)
+                                ->orderBy('sp.expiry_date','ASC')
+                                ->groupBy('s.email');       
         
-        $organizations_expiring_soon = $organizations_expiring_query->simplePaginate(10,['*'],'organizations');
-        return view('subscribers.expiring', compact('organizations_expiring_soon'));
+        $members_expiring_soon = $members_expiring_query->simplePaginate(10,['*'],'members');
+        
+        return view('subscribers.expiring', compact('members_expiring_soon'));
     }
 
     public function export(){
